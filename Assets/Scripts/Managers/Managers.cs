86697fa71ba3c7de949642cecda6c5048ed2e271
KJH <mallangyi@naver.com>
2021-09-27 21:47:26 +0900

    cameraFinish

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class Managers : MonoBehaviour
 {
-
-    static Managers s_instance;
-    public static Managers Instance { get { init();  return s_instance; } }
+    static Managers s_instance; // 유일성이 보장된다
+    static Managers Instance { get { Init(); return s_instance; } } // 유일한 매니저를 갖고온다
 
     InputManager _input = new InputManager();
-    public static InputManager input { get { return Instance._input; } }
+    ResourceManager _resource = new ResourceManager();
+
+    public static InputManager Input { get { return Instance._input; } }
+    public static ResourceManager Resource { get { return Instance._resource; } }
+
     // Start is called before the first frame update
     void Start()
     {
-        init();
-    }
+        Init();
+	}
 
     // Update is called once per frame
     void Update()
@@ -22,20 +25,19 @@ public class Managers : MonoBehaviour
         _input.OnUpdate();
     }
 
-    static void init()
+    static void Init()
     {
         if (s_instance == null)
         {
-            GameObject go = GameObject.Find("@Managers");
-            if( go == null)
+			GameObject go = GameObject.Find("@Managers");
+            if (go == null)
             {
-                go = new GameObject { name = "@Managers " };
+                go = new GameObject { name = "@Managers" };
                 go.AddComponent<Managers>();
             }
 
             DontDestroyOnLoad(go);
             s_instance = go.GetComponent<Managers>();
-
-        }
-    }
+		}		
+	}
 }
